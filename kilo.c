6536cf4bbd87debe9761dd01f0a7743fdda2ad33
Luxe17 <hanson1717@live.com>
2025-02-01 13:03:40 -0800

    Add functionality to turn off output processing and software control flow

--- kilo.c
+++ kilo.c
@@ -12,22 +12,27 @@ void die(const char *s){
     exit(1);
 }
 
-void disableRawMode() {
+void disableRawMode(void) {
     if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
         die("tcsetattr");
 }
 
-void enableRawMode(){
+void enableRawMode(void){
+    // Get the terminal attributes
     if(tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode);
-
+    
+    // Modify the struct to turn on raw mode
     struct termios raw = orig_termios;
     raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_iflag &= ~(IXON);
 
+    // Set the terminal attributes
     if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
-int main(){
+int main(void){
     enableRawMode();
 
     while(1){
